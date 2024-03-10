b77223af8ad180085d674e690432dd3f184e0f80
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-03-10 18:06:05 -0500

    Install Flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'wiki-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'students-wiki';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
